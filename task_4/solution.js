3d98373a7c8eaa8c6893420940642ce7ef58bc6e
Natalya Lukyanova <n.lukyanova@avgust.com>
2022-12-15 12:59:46 +0300

    Задание 4 правка

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,7 +3,7 @@
 function parseProducts(json)
  {
     let data = JSON.parse(json);
-    let products = data.products
+    let products = data.products;
     return products;
  }
 
@@ -12,14 +12,10 @@ function renderProductsCards(json)
     clearProducts();
     let products = parseProducts(json);
     let length = pr.length;
-    for(let i = 0; i < length; i++)
+    for (let i = 0; i < length; i+=1)
     {
         addProduct(products[i]);
     }
 }
  
-// Выполните функцию clearProducts - она уже написана в коде, просто вызовите ее, аргументы не требуются
-// Выполните функцию parseProducts, передав в неё json - аргумент функции и запишите результат в переменную products
-// Запишите в переменную length значение свойства products.length
-// Напишите цикл, в котором перебираете все products от 0 до (length - 1)
-// и выполняете addProduct с каждым элементом массива products (передавая элемент массива как аргумент)
+
