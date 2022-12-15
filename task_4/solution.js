1d8eede7a152017c9499a959b6ed75555b2fe6d8
Natalya Lukyanova <n.lukyanova@avgust.com>
2022-12-15 13:11:49 +0300

    Задание 4 испр. 2

--- task_4/solution.js
+++ task_4/solution.js
@@ -11,7 +11,7 @@ function renderProductsCards(json)
 {
     clearProducts();
     let products = parseProducts(json);
-    let length = pr.length;
+    let length = products.length;
     for (let i = 0; i < length; i+=1)
     {
         addProduct(products[i]);
